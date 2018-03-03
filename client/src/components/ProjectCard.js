9d7d3a4a761a232ef1f0f032eec176d7b4ea0af6
Francisco Bento <franciscobdsjunior@gmail.com>
2018-03-03 12:36:30 -0300

    :lipstick: Update image upload input

--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -5,7 +5,7 @@ import { Card, Icon, Image } from 'semantic-ui-react';
 const ProjectCard = ({title, description, value, image, created_at, user}) => {
   return (
     <Card fluid>
-     <Image alt='image' src='http://via.placeholder.com/600x150' />
+     <Image alt='image' src={image ? image : 'http://via.placeholder.com/100x300'} />
      <Card.Content>
        <Card.Header>{title}</Card.Header>
        <Card.Meta>{moment(created_at).fromNow()} by {user.username}</Card.Meta>
--- client/src/containers/CreateProjectPage.js
+++ client/src/containers/CreateProjectPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Form, Button, Segment } from 'semantic-ui-react';
+import { Container, Form, Button, Dimmer, Image, Segment, Header as SemanticHeader } from 'semantic-ui-react';
 import Header from '../components/Header';
 import api from '../utils/api';
 
@@ -9,11 +9,12 @@ class CreateProjectPage extends Component {
     description: '',
     value: '',
     image: '',
+    fileUpload: null,
+    dimmerActive: false,
     loading: false
   }
 
   __handleSubmit = (e) => {
-
     if (!this.state.title || !this.state.description || !this.state.value || !this.state.image ) {
       return;
     }
@@ -36,6 +37,21 @@ class CreateProjectPage extends Component {
     })
   }
 
+  __onUpdateImage = () => {
+    const file = this.state.fileUpload.files[0];
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      this.setState({image: reader.result});
+    }
+    reader.readAsDataURL(file);
+  }
+
+  __handleAddImage = () => {
+    if (this.state.fileUpload) {
+      this.state.fileUpload.click()
+    }
+  }
+
   render () {
     return (
       <div>
@@ -43,6 +59,21 @@ class CreateProjectPage extends Component {
         <Container text style={{ marginTop: '7em' }}>
           <Segment>
           <Form onSubmit={this.__handleSubmit} loading={this.state.loading}>
+            <Form.Field>
+              <label>Image</label>
+              <input hidden type='file' ref={(ref) => {this.setState({fileUpload: ref})}} onChange={this.__onUpdateImage} placeholder='Image' required />
+              <Dimmer.Dimmable dimmed={this.state.dimmerActive}
+                onMouseEnter={() => this.setState({dimmerActive: true})}
+                onMouseLeave={() => this.setState({dimmerActive: false})} >
+                <Dimmer active={this.state.dimmerActive}>
+                  <SemanticHeader as='h2' inverted>
+                    Add an image for you project
+                  </SemanticHeader>
+                  <Button primary as='a' onClick={this.__handleAddImage}>Add</Button>
+                </Dimmer>
+              <Image fluid src={this.state.image ? this.state.image : 'http://via.placeholder.com/700x300'} />
+              </Dimmer.Dimmable>
+            </Form.Field>
             <Form.Field>
               <label>Title</label>
               <input onChange={(e) => { this.setState({title: e.target.value })}} placeholder='Title' required />
@@ -54,10 +85,6 @@ class CreateProjectPage extends Component {
             <Form.Field>
               <label>Value</label>
               <input onChange={(e) => { this.setState({value: e.target.value })}} type='number' max='500' placeholder='Value' required />
-            </Form.Field>
-            <Form.Field>
-              <label>Image</label>
-              <input onChange={(e) => { this.setState({image: e.target.value })}} placeholder='Image' required />
             </Form.Field>
             <Button type='submit'>Create</Button>
           </Form>
