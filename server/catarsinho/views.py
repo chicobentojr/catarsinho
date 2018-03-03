d8fb9c1a3306949d8ce05c3c79c43a96acbf126b
Francisco Bento <franciscobdsjunior@gmail.com>
2018-03-03 07:27:32 -0300

    :lock: Update API permissions

--- server/catarsinho/views.py
+++ server/catarsinho/views.py
@@ -1,20 +1,18 @@
 from django.contrib.auth.models import User
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from catarsinho.serializers import UserSerializer, ProjectSerializer
 from catarsinho.models import Project
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+
+    permission_classes = (permissions.AllowAny,)
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
+
+    permission_classes = (permissions.AllowAny,)
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
--- server/server/urls.py
+++ server/server/urls.py
@@ -10,7 +10,7 @@ router.register(r'projects', views.ProjectViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', obtain_jwt_token)
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/auth/', obtain_jwt_token),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
